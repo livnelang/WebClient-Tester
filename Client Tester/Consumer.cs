6f9d4939ca524747561c4e461c72d0aa6a51a025
Livne Lang <livne@alphasrc.com>
2016-01-13 17:02:48 +0200

    First Commit

---
+++ Client Tester/Consumer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_Tester
+{
+    class Consumer
+    {
+        string base64String;
+        string filename;
+        /*
+         * Consumer Constructor 
+         */
+        public Consumer(string base_64, string fname)
+        {
+            base64String = base_64;
+            filename = fname;
+        }
+
+        /*
+         * Send File to the server
+         */
+        public void sendFile()
+        {
+
+            /* new WebClient  */
+            WebClient client = new WebClient();
+            /* Collection values */
+            var values = new NameValueCollection();
+            values["base64"] = base64String;
+            values["filename"] = filename;
+            try { 
+                /* Handle the response */
+                var response = client.UploadValues("https://boardcast-ws.herokuapp.com/decode64", values);
+                var responseString = Encoding.Default.GetString(response);
+                Console.WriteLine(responseString);
+            }
+            catch (WebException e)
+            {
+                //How do I capture this from the UI to show the error in a message box?
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+    }
+}
---
+++ Client Tester/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client_Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "C:\\Users\\Livne\\Desktop\\images_check\\google.png";
+            string s2 = "C:\\Users\\Livne\\Desktop\\images_check\\ynet.png";
+
+            Program p1  = new Program(); // new instance
+            byte[] base_1 = p1.FileToByteArray(s1);
+            byte[] base_2 = p1.FileToByteArray(s2);
+            byte[] base_3 = base_1;
+
+            string base64String = System.Convert.ToBase64String(base_1);
+
+            /* Create the Consumers */
+            Consumer c1 = new Consumer(base64String, "g1.png");
+            Consumer c2 = new Consumer(base64String, "g2.png");
+            Consumer c3 = new Consumer(base64String, "g3.png");
+
+
+            Thread consumer1 = new Thread(new ThreadStart(c1.sendFile));
+            Thread consumer2 = new Thread(new ThreadStart(c2.sendFile));
+            Thread consumer3 = new Thread(new ThreadStart(c3.sendFile));
+            try {
+                consumer1.Start();
+                consumer2.Start();
+                consumer3.Start();
+            }
+            catch (ThreadStateException e) {
+                Console.WriteLine(e);  // Display text of exception
+            }
+
+        }
+        /*
+         * Convert file name into byte array
+         */
+        public byte[] FileToByteArray(string fileName)
+        {
+            byte[] buff = null;
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            long numBytes = new FileInfo(fileName).Length;
+            buff = br.ReadBytes((int)numBytes);
+            return buff;
+        }
+    }
+}
